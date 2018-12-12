82a75697ea0bcd9cf95961f77aedd271440125cf
Toni Ortega <toni.47.ortega@gmail.com>
2018-12-12 22:55:54 +0800

    TO DO list in index.js file

---
+++ index.js
@@ -0,0 +1,46 @@
+/*
+ * Primary file for the API
+ *
+ */
+
+// Dependencies
+
+// Instantiate HTTP server
+
+// Start HTTP server
+
+// Logic for HTTP server
+
+  // Get the url and parse it
+
+  // Get the path of the url
+
+  // Get the query string as an object
+
+  // Get the HTTP method
+
+  // Get the headers as an object
+
+  // Get the payload, if any
+
+    // Choose the handler this request should go to. If one is not found, choose the notFound hendler
+
+    // Construct the data object to send to the handler
+
+    // Route the request to the handler specified in the router
+
+      // Use the status code called back by the handler, or default to 200
+
+      // Use the payload called back by the handler, or default to empty object
+
+      // Convert the payload to a string
+
+      // Return the response
+
+// Define the handlers
+
+// hello handler
+
+// Not found handler
+
+// Define request router
