35e2256c5bf312f9f67d52735d6645aa343e44bd
Toni Ortega <toni.47.ortega@gmail.com>
2018-12-12 23:01:20 +0800

    Created config.js file with TO DO list in it

---
+++ config.js
@@ -0,0 +1,16 @@
+/*
+ * Crteate and export configuration variables
+ *
+ */
+
+// Container for all the environments
+
+// Staging (default) environment
+
+// Production environment
+
+// Determine which environment was passed as a command-line argument
+
+// Check that the current environment is one of the environments above, if not, default to staging
+
+// Export the module
